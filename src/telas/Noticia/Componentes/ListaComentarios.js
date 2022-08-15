72d8614ab37de5be1e47c2f9fca225524f82b09e
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2022-08-15 10:43:18 -0300

    Pequena correção

--- src/telas/Noticia/Componentes/ListaComentarios.js
+++ src/telas/Noticia/Componentes/ListaComentarios.js
@@ -15,7 +15,7 @@ const DATA = [
         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
         imagem: ImgTeste,
         name: 'Rebeca',
-        comentario: 'Mutio bom esse app nossa que app magnifico oi buceta' 
+        comentario: 'Mutio bom esse app nossa que app magnifico oi hexa campeao' 
     },
     {
         id: '58694a0f-3da1-471f-bd96-145571e29d72',
