82ed618c87120c482b5838f0c64f1d112032f265
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2022-08-23 11:44:10 -0300

    Configurando telas de perfil e tela de cadastro

--- src/telas/CadastroComum/componente/Forms.js
+++ src/telas/CadastroComum/componente/Forms.js
@@ -1,40 +1,72 @@
 import React, { useState } from "react";
-import { SafeAreaView, View, TextInput, StyleSheet } from "react-native";
+import { SafeAreaView, View, TextInput, StyleSheet, TouchableOpacity, Image } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import { Button } from "react-native-paper";
+import imgPerson from '../../../assets/profilePhoto.png'
 
 
 export default function Forms() {
-    const [comentário, setComentario] = useState("")
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const [senhaAgain, setsenhaAgain] = useState("")
+    const [image, setImage] = useState(imgPerson)
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            quality: 1,
+        });
+
+        console.log(result.uri)
+        setImage(result.uri)
+    }
+
+    function sendToDatabase() {
+        if (nome || email || senha || senhaAgain === "") {
+            alert('Preencha corretamente todos os campos')
+        } else {
+            alert("Save at database " + nome + '-' + email + '-' + senha + '-' + senhaAgain + '-' + image)
+        }
+
+    }
+
 
     return <>
         <SafeAreaView>
             <View>
+                <Button onPress={pickImage}>Selecionar foto</Button>
                 <TextInput
                     placeholder="Nome"
                     style={styles.input}
-                    onChangeText={setComentario}
-                    value={comentário}
+                    onChangeText={setNome}
+                    value={nome}
                 />
 
                 <TextInput
                     placeholder="Email"
                     style={styles.input}
-                    onChangeText={setComentario}
-                    value={comentário}
+                    onChangeText={setEmail}
+                    value={email}
                 />
 
                 <TextInput
                     placeholder="Senha"
                     style={styles.input}
-                    onChangeText={setComentario}
-                    value={comentário}
+                    onChangeText={setSenha}
+                    value={senha}
                 />
 
                 <TextInput
                     placeholder="Insira novamente sua senha"
                     style={styles.input}
-                    onChangeText={setComentario}
-                    value={comentário}
+                    onChangeText={setsenhaAgain}
+                    value={senhaAgain}
                 />
+
+                <Button onPress={() => {
+                    sendToDatabase()
+                }}>Cadastrar</Button>
 
             </View>
         </SafeAreaView>
@@ -46,10 +78,19 @@ const styles = StyleSheet.create({
         height: 40,
         width: '80%',
         margin: 12,
+        alignSelf: 'center',
         borderWidth: 1,
         padding: 10,
+        borderRadius: 8
     },
     iconSendComment: {
         marginTop: 18
+    },
+    img: {
+        width: 145,
+        height: 145,
+        borderRadius: 64,
+        alignSelf: 'center',
+        marginTop: 16
     }
 })
--- src/telas/CadastroComum/index.js
+++ src/telas/CadastroComum/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import Topo from './componente/Topo'
+
 import Forms from './componente/Forms'
 
 export default function CadastroComum(props) {
     return <>
         <SafeAreaView>
             <Forms />
-            <Topo props={props} />
-
         </SafeAreaView>
     </>
 }
